ccb5d9eb8bfe68d4e97e89fa831163e0950ae9d2
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-04-13 15:57:43 +0100

    Configured redux
    Creacted action creators and action-types
    Filtered the data into continents by creating contenet module
    Configured the reducer
    Configured the store using redux-thunk, aplay middleware and createStore

---
+++ src/redux/actionTypes.js
@@ -0,0 +1,5 @@
+export const LOAD_DATA = 'LOAD_DATA';
+export const SET_CONTINENT = 'SET_CONTINENT';
+export const SET_COUNTRY = 'SET_COUNTRY';
+
+// export const LOAD_FOR_SPECIFIC_DATE = 'LOAD_FOR_SPECIFIC_DATE';
